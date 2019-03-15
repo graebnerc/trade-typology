17611a13d36b678a03755a6fab94fbfc766ef41c
Claudius Graebner <claudius@claudius-graebner.com>
2019-03-15 13:12:48 +0100

    Add SITC codes for export shares; implement primary good share

--- src/data_preparation.R
+++ src/data_preparation.R
@@ -40,6 +40,24 @@ exp_to_gdp <- exp_to_gdp_raw[, exp_to_gdp:=ne.trd.gnfs.zs
 # Coals, Metal and Oil shares of total exports=================================
 # For SITC codes see: 
 # https://unstats.un.org/unsd/tradekb/Knowledgebase/50262/Search-SITC-code-description
+oil_codes <- c("33", "34") # 33	Petroleum, petroleum products and related materials and 34	Gas, natural and manufactured
+coal_and_metal_codes_2 <- c("32", "35", "28", "68", "97")
+# 32	Coal, coke and briquettes
+# 35	Electric current
+# 28	Metalliferous ores and metal scrap
+# 68	Non-ferrous metals
+# 97	Gold, non-monetary (excluding gold ores and concentrates)
+coal_and_metal_codes_4 <- c("5224", "5231", "5232", "5233")
+# 5224	Metallic oxides of zinc, iron, lead, chromium etc
+# 5231	Metallic salts and peroxysalts of inorganic acids
+# 5232	Metallic salts and peroxysalts of inorganic acids
+# 5233	Salts of metallic acids; compounds of precious metals
+coal_and_metal_codes_5 <- c("52217")
+# 52217 Alkali and alkaline-earth metals; rare earth metals
+# Eher nicht:
+# 69	Manufactures of metals, nes
+# 691	Structures and parts, nes, of iron, steel or aluminium
+
 export_data_file_name <- "data/hrvd_complexity_atlas.csv.gz"
 if (update_data){
   web_link <- "https://intl-atlas-downloads.s3.amazonaws.com/country_sitcproduct4digit_year.csv.zip"
@@ -52,7 +70,30 @@ if (update_data){
   export_data_raw <- fread(cmd = paste0("gunzip -c ", export_data_file_name), 
                            colClasses = c(rep("double", 2), rep("character", 2)))
 }
-export_data <- export_data_raw
+
+export_data <- export_data_raw[, 
+                               total_exports:=sum(export_value, na.rm = T), 
+                               .(location_code, year)]
 
 # Share of primary exports=====================================================
+
+primary_goods_codes_1 <- c("0", "1", "2", "4")
+primary_goods_codes_2 <- c(primary_goods_codes_1, "3")
+# In jedem Fall:
+# 0	Food and live animals chiefly for food
+# 2	Crude materials, inedible, except fuels
+# 1	Beverages and tobacco
+# 4	Animal and vegetable oils, fats and waxes
+# Unklar:
+# 3	Mineral fuels, lubricants and related materials
+
+primary_exports_data <- export_data[, sitc_main:=substr(sitc_product_code, 1, 1)
+                                    ][, export_primary:=sum(export_value, na.rm = T), 
+                                      .(year, location_code, sitc_main)
+                                      ][, primary_exports_share:=export_primary/total_exports]
+primary_exports_data <-  primary_exports_data[, 
+                                              .(year, location_code, primary_exports_share)]
 # Merging data=================================================================
+nat_res_rents
+exp_to_gdp
+primary_exports_data
