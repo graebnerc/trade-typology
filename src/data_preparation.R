d7e6d3a8d33c5e2017031b45573ce704abaaae83
Claudius Graebner <claudius@claudius-graebner.com>
2019-03-15 11:23:40 +0100

    Download export data from Harvard Atlas

--- src/data_preparation.R
+++ src/data_preparation.R
@@ -1,5 +1,6 @@
 # 15.03.2019: Create new data for endownment dimension
 rm(list = ls())
+library(countrycode)
 library(data.table)
 library(WDI)
 update_data <- T
@@ -37,5 +38,21 @@ exp_to_gdp <- exp_to_gdp_raw[, exp_to_gdp:=ne.trd.gnfs.zs
                              ][, .(iso2c, year, exp_to_gdp)]
 
 # Coals, Metal and Oil shares of total exports=================================
+# For SITC codes see: 
+# https://unstats.un.org/unsd/tradekb/Knowledgebase/50262/Search-SITC-code-description
+export_data_file_name <- "data/hrvd_complexity_atlas.csv.gz"
+if (update_data){
+  web_link <- "https://intl-atlas-downloads.s3.amazonaws.com/country_sitcproduct4digit_year.csv.zip"
+  export_data_raw <- fread(cmd = paste0("curl ", web_link, " | funzip"),
+                           colClasses = c(rep("double", 11), rep("character", 4)), 
+                           select = c("year", "export_value", "location_code", "sitc_product_code"))
+  export_data_raw <- export_data_raw[location_code%in%countrycode(countries_considered, "iso2c", "iso3c")]
+  readr::write_csv(export_data_raw, gzfile(export_data_file_name))
+} else{
+  export_data_raw <- fread(cmd = paste0("gunzip -c ", export_data_file_name), 
+                           colClasses = c(rep("double", 2), rep("character", 2)))
+}
+export_data <- export_data_raw
+
 # Share of primary exports=====================================================
 # Merging data=================================================================
