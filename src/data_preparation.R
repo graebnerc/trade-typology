579463b225ae373259d816aeb4e959e31e457b8b
Claudius Graebner <claudius@claudius-graebner.com>
2019-03-15 15:13:20 +0100

    Fix problem with non-copied data tables; added export data

--- src/data_preparation.R
+++ src/data_preparation.R
@@ -3,7 +3,7 @@ rm(list = ls())
 library(countrycode)
 library(data.table)
 library(WDI)
-update_data <- T
+update_data <- F
 
 countries_considered <- strsplit(
   "LU, SE, FI, DK, FR, NL, BE, SI, DE, AT, LV, EE, SK, CZ, PL, HU, GB, IE, PT, GR, ES, IT", 
@@ -22,7 +22,8 @@ if (update_data){
 }
 
 nat_res_rents <- nat_res_rents_raw[, res_rents:=ny.gdp.totl.rt.zs
-                                   ][, .(iso2c, year,res_rents)]
+                                   ][, iso2c:=countrycode(iso2c, "iso2c", "iso3c")
+                                     ][, .(iso2c, year,res_rents)]
 
 # Exports to GDP===============================================================
 # https://data.worldbank.org/indicator/ne.trd.gnfs.zs
@@ -37,7 +38,8 @@ if (update_data){
 }
 
 exp_to_gdp <- exp_to_gdp_raw[, exp_to_gdp:=ne.trd.gnfs.zs
-                             ][, .(iso2c, year, exp_to_gdp)]
+                             ][, iso2c:=countrycode(iso2c, "iso2c", "iso3c")
+                               ][, .(iso2c, year, exp_to_gdp)]
 
 # Get export data==============================================================
 # http://atlas.cid.harvard.edu/downloads
@@ -55,9 +57,8 @@ if (update_data){
                            colClasses = c(rep("double", 2), rep("character", 2)))
 }
 
-export_data <- export_data_raw[, 
-                               total_exports:=sum(export_value, na.rm = T), 
-                               .(location_code, year)]
+export_data_raw[, total_exports:=sum(export_value, na.rm = T), 
+                .(location_code, year)]
 
 # Oil shares of total exports=================================
 
@@ -66,7 +67,20 @@ export_data <- export_data_raw[,
 oil_codes <- c("33", "34") 
 # 33	Petroleum, petroleum products and related materials 
 # 34	Gas, natural and manufactured
+oil_exports <- copy(export_data_raw)
+oil_exports[, sitc_red:=substr(sitc_product_code, 1, 2)
+                           ][sitc_red%in%oil_codes, 
+                             oil_export:=sum(export_value, na.rm = T), 
+                             .(year, location_code)
+                             ][, oil_exports_share:=oil_export/total_exports]
+oil_exports <- unique(oil_exports[!is.na(oil_exports_share)
+                           ][, .(year, location_code, oil_exports_share)])
+head(oil_exports)
 
+# Coal and metal share of total exports========================================
+
+# For SITC codes see: 
+# https://unstats.un.org/unsd/tradekb/Knowledgebase/50262/Search-SITC-code-description
 coal_and_metal_codes_2 <- c("32", "35", "28", "68", "97")
 # 32	Coal, coke and briquettes
 # 35	Electric current
@@ -74,32 +88,29 @@ coal_and_metal_codes_2 <- c("32", "35", "28", "68", "97")
 # 68	Non-ferrous metals
 # 97	Gold, non-monetary (excluding gold ores and concentrates)
 
-oil_exports <- export_data[, sitc_red:=substr(sitc_product_code, 1, 2)
-                           ][sitc_red%in%oil_codes, 
-                             oil_export:=sum(export_value, na.rm = T), 
-                             .(year, location_code)
-                             ][, oil_exports_share:=oil_export/total_exports
-                               ][!is.na(oil_exports_share)
-                                 ][, .(year, location_code, oil_exports_share)]
-oil_exports <- unique(oil_exports)
-head(oil_exports)
-
-# Coal and metal share of total exports========================================
-
-# For SITC codes see: 
-# https://unstats.un.org/unsd/tradekb/Knowledgebase/50262/Search-SITC-code-description
 coal_and_metal_codes_4 <- c("5224", "5231", "5232", "5233")
 # 5224	Metallic oxides of zinc, iron, lead, chromium etc
 # 5231	Metallic salts and peroxysalts of inorganic acids
 # 5232	Metallic salts and peroxysalts of inorganic acids
 # 5233	Salts of metallic acids; compounds of precious metals
 
-coal_and_metal_codes_5 <- c("52217")
-# 52217 Alkali and alkaline-earth metals; rare earth metals
 # Eher nicht:
 # 69	Manufactures of metals, nes
 # 691	Structures and parts, nes, of iron, steel or aluminium
-
+coal_metal_shares <- copy(export_data_raw)
+coal_metal_shares[, sitc2:=substr(sitc_product_code, 1, 2)
+                  ][, sitc4:=sitc_product_code
+                    ][, coal_metal:=ifelse(
+                      sitc2 %in% coal_and_metal_codes_2 | 
+                        sitc4 %in% coal_and_metal_codes_4, 
+                      TRUE, FALSE)]
+coal_metal_shares[coal_metal==TRUE,
+                  coal_metal_exports:=sum(export_value, na.rm = T), 
+                  .(year, location_code)
+                  ][ , coal_metal_export_share:=coal_metal_exports/total_exports]
+coal_metal_shares <- unique(coal_metal_shares[!is.na(coal_metal_export_share), 
+                                              .(year, location_code, coal_metal_export_share)])
+head(coal_metal_shares)
 
 # Share of primary exports=====================================================
 
@@ -113,15 +124,30 @@ primary_goods_codes_2 <- c(primary_goods_codes_1, "3")
 # Unklar:
 # 3	Mineral fuels, lubricants and related materials
 # TODO das stimmt noch nicht: nicht die subsets genommen, und gibt summe>100
-primary_exports_data <- export_data[, sitc_main:=substr(sitc_product_code, 1, 1)
-                                    ][, export_primary:=sum(export_value, na.rm = T), 
-                                      .(year, location_code, sitc_main)
-                                      ][, primary_exports_share:=export_primary/total_exports]
-primary_exports_data <-  primary_exports_data[, 
-                                              .(year, location_code, primary_exports_share)]
+primary_exports_data <- copy(export_data_raw)
+primary_exports_data[, sitc_main:=substr(sitc_product_code, 1, 1)
+                     ][sitc_main%in%primary_goods_codes_1, 
+                       export_primary_1:=sum(export_value, na.rm = T), 
+                       .(year, location_code)
+                       ][sitc_main%in%primary_goods_codes_2, 
+                         export_primary_2:=sum(export_value, na.rm = T), 
+                         .(year, location_code)
+                       ][, primary_exports_share_1:=export_primary_1/total_exports
+                         ][, primary_exports_share_2:=export_primary_2/total_exports]
+primary_exports_data <-  unique(
+  primary_exports_data[!is.na(primary_exports_share_1) & !is.na(primary_exports_share_2), 
+                       .(year, location_code, primary_exports_share_1, primary_exports_share_2)]
+  )
+head(primary_exports_data)
 
 # Merging data=================================================================
-nat_res_rents
-exp_to_gdp
-primary_exports_data
-oil_exports
+head(nat_res_rents)
+head(exp_to_gdp)
+head(primary_exports_data)
+head(oil_exports)
+head(coal_metal_shares)
+full_data <- dplyr::full_join(coal_metal_shares, oil_exports, by=c("year", "location_code")) %>%
+  dplyr::full_join(., primary_exports_data, by=c("year", "location_code")) %>%
+  dplyr::full_join(., exp_to_gdp, by=c("year", "location_code"="iso2c")) %>%
+  dplyr::full_join(., nat_res_rents, by=c("year", "location_code"="iso2c"))
+fwrite(full_data, "data/dimension_endownment_data.csv")
