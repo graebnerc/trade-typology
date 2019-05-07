70417bdaa7ffbd25f9be6d6c68f1f1158a835cff
Claudius Graebner <claudius@claudius-graebner.com>
2019-05-07 16:56:19 +0200

    Implemented nb of clusters test with 17 measures that work

--- src/nb_clusters.R
+++ src/nb_clusters.R
@@ -1,28 +1,37 @@
-library(factoextra)
-library(NbClust)
-
-head(cluster_data)
-head(cluster_vars)
-
 cluster_data_nb <- cluster_data %>%
   select(one_of("country", cluster_vars))
 cluster_data_nb <- as.data.frame(cluster_data_nb)
 rownames(cluster_data_nb) <- cluster_data_nb$country
 cluster_data_nb["country"] <- NULL
 
-# Elbow method
-fviz_nbclust(df, kmeans, method = "wss") +
-  geom_vline(xintercept = 4, linetype = 2)+
-  labs(subtitle = "Elbow method")
+recommended_nb <- NbClust::NbClust(cluster_data_nb, distance = "euclidean", 
+                                   method = "ward.D", 
+                                   index = c("kl", "ch", "cindex", "db", 
+                                             "silhouette", "duda", "pseudot2", 
+                                             "beale", "ratkowsky", "mcclain",
+                                             "ptbiserial", "gap",  "gamma", 
+                                             "gplus", "tau", "sdindex", "sdbw"
+                                             ))
+recommended_nb_hist_data <- data.frame(
+  method=names(recommended_nb$Best.nc[1,]),
+  clusters=recommended_nb$Best.nc[1,]
+  )
 
-# Silhouette method
-fviz_nbclust(df, kmeans, method = "silhouette")+
-  labs(subtitle = "Silhouette method")
-
-# Gap statistic
-# nboot = 50 to keep the function speedy. 
-# recommended value: nboot= 500 for your analysis.
-# Use verbose = FALSE to hide computing progression.
-set.seed(123)
-fviz_nbclust(df, kmeans, nstart = 25,  method = "gap_stat", nboot = 50)+
-  labs(subtitle = "Gap statistic method")
+recommended_nb_hist <- ggplot(recommended_nb_hist_data, 
+                              aes(x=clusters
+                                  )) + 
+  geom_histogram(binwidth = 1, color="#ffffff") +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 8), breaks = 0:8) +
+  scale_x_continuous(breaks = 1:16) +
+  ggtitle("Nb of clusters based on 17 indicators") +
+  xlab("Recommended nb of clusters") +
+  ylab("Nb of recommendations") +
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        axis.line = element_line(), 
+        panel.grid.minor = element_blank(),
+        legend.position = "none")
+recommended_nb_hist
+ggsave(plot = recommended_nb_hist, 
+       filename = "output/nb_clusters.pdf",
+       height = 4, width = 6)
