4fbd0ff05470628bc2f9a9ae57f95d297dd99878
Claudius Graebner <claudius@claudius-graebner.com>
2019-05-23 18:45:26 +0200

    Do not make taxonomy plots by default

--- src/clustering.R
+++ src/clustering.R
@@ -7,6 +7,7 @@ library(factoextra)
 library(NbClust)
 library(cluster)
 # TODO Identify source of difference with branch master
+make_taxonomy_plots <- FALSE # Set to T to make taxonomy plots as in JEE (but not yet updated)
 # Function definitions=========================================================
 source("src/clustering_functions.R")
 
@@ -118,6 +119,7 @@ table_order <- c("empl_agr",
                  "kof_econ_dejure")
 
 taxonomy_table_data <- data_taxonomy %>%
+  dplyr::mutate(average_wages=average_wages/1000) %>%
   dplyr::select(-country) %>%
   dplyr::group_by(cluster) %>%
   dplyr::summarise_all(mean) %>%
@@ -138,83 +140,84 @@ write(
 
 # Taxonomy plots (TO BE DONE)--------------------------------------------------
 # TODO Adjust these plots to new results
-
-# Cluster 1: primary goods (Latvia and Estonia)
-#  rents of natural resources are high (aber: vielleicht nur wegen niedrigem GDP?)
-#  share of oil exports is high
-#  complexity is low
-#  low investments in research and development
-#  The government expenditure on education is surprisingly high
-#  low degree of wage coordination and low government expenditures on social security.
-#  very low corporate, estate and all other wealth tax revenues are also remarkable
-#  FDI as in high road
-cluster_1_vars <- c(
-  "res_rents", "oil_exports_share", "primary_exports_share_1",
-  "complexity_harv", "gerd", "gov_exp_educ", "industrial_to_gdp",
-  "coord", "gov_exp_socprtc",
-  "tax_corpcap", "tax_estate_plus_wealth", "size_of_finance"
-)
-
-# Cluster 2: finance (Luxembourg)
-#  vast size of its financial sector (the share of the financial sector)
-#  share of foreign direct investments 
-#  lowest share of primary goods in exports
-#  weak regulation  
-#  highest degree of economic globalisation in terms of de facto openness as well as in terms of de jure openness. 
-#  high corporate tax revenues, and high benefits in the case of unemployment.
-cluster_2_vars <- c(
-  "kof_econ_defacto", "coal_metal_export_share", "oil_exports_share", 
-  "primary_exports_share_1", "res_rents", # TODO Share of coals and metals raus?
-  "ict_ksh", "ubr", "gini_market", 
-  "tax_corpcap", "tax_estate_plus_wealth", "fdi_to_gdp", 
-  "size_of_finance", "kof_econ_dejure"
-)
-
-# Cluster 3: deregulated regime (United Kingdom, Ireland and Hungary)
-#  deregulated labour market and high income inequality. 
-#  43.5 percent of their former net income in case of unemployment and the employment protection (mean value of 1.5) is also very low. 
-#  low government expenditures on social security
-#  low tax revenues can be described as a strategy aiming to improve price competitiveness.
-cluster_3_vars <- c(
-  "coal_metal_export_share", 
-  "ict_ksh",
-  "employment_protect", "gini_market", "ubr"
-)
-
-# Cluster 4: less globalised model (Poland, Greece, Portugal, Slovakia, Spain, Italia, Slovenia and Czech Republic)
-#  exact counterpart to cluster 2 (see Table 2)
-#  low degree of international integration as reflected by low values in foreign direct investments and the economic globalisation index. 
-#  government expenditures on education and investments in research and development are considerably below the other trade regimes. 
-#  relatively strict labor market regulation.
-cluster_4_vars <- c(
-  "kof_econ_defacto", 
-  "gov_exp_educ",
-  "employment_protect",
-  "kof_econ_dejure", "fdi_to_gdp"
-)
-
-# Cluster 5: Technological leaders (France, Sweden, Finland, Denmark, Netherlands, Belgium, Germany and Austria)
-#  highest degree of economic complexity
-#  high degree of wage coordination and high government expenditures on social security.
-#  high investments in research and development
-cluster_5_vars <- c(
-  "complexity_harv", "gerd", "industrial_to_gdp", 
-  "coord", "gov_exp_socprtc"
-)
-
-clustering_vars <- list(
-  "C1" = cluster_1_vars,
-  "C2" = cluster_2_vars,
-  "C3" = cluster_3_vars,
-  "C4" = cluster_4_vars,
-  "C5" = cluster_5_vars
-)
-
-for (i in 1:5){
-  cluster_considered <- paste0("C", i)
-  print(cluster_considered)
-  current_plots <- make_plots(data_taxonomy, cluster_considered, clustering_vars)
-  ggsave(plot = current_plots, 
-         filename = paste0("output/", cluster_considered, "_barplot.pdf"), 
-         width = length(current_plots)*2.5, height = 8)
+if (make_taxonomy_plots){
+  # Cluster 1: primary goods (Latvia and Estonia)
+  #  rents of natural resources are high (aber: vielleicht nur wegen niedrigem GDP?)
+  #  share of oil exports is high
+  #  complexity is low
+  #  low investments in research and development
+  #  The government expenditure on education is surprisingly high
+  #  low degree of wage coordination and low government expenditures on social security.
+  #  very low corporate, estate and all other wealth tax revenues are also remarkable
+  #  FDI as in high road
+  cluster_1_vars <- c(
+    "res_rents", "oil_exports_share", "primary_exports_share_1",
+    "complexity_harv", "gerd", "gov_exp_educ", "industrial_to_gdp",
+    "coord", "gov_exp_socprtc",
+    "tax_corpcap", "tax_estate_plus_wealth", "size_of_finance"
+  )
+  
+  # Cluster 2: finance (Luxembourg)
+  #  vast size of its financial sector (the share of the financial sector)
+  #  share of foreign direct investments 
+  #  lowest share of primary goods in exports
+  #  weak regulation  
+  #  highest degree of economic globalisation in terms of de facto openness as well as in terms of de jure openness. 
+  #  high corporate tax revenues, and high benefits in the case of unemployment.
+  cluster_2_vars <- c(
+    "kof_econ_defacto", "coal_metal_export_share", "oil_exports_share", 
+    "primary_exports_share_1", "res_rents", # TODO Share of coals and metals raus?
+    "ict_ksh", "ubr", "gini_market", 
+    "tax_corpcap", "tax_estate_plus_wealth", "fdi_to_gdp", 
+    "size_of_finance", "kof_econ_dejure"
+  )
+  
+  # Cluster 3: deregulated regime (United Kingdom, Ireland and Hungary)
+  #  deregulated labour market and high income inequality. 
+  #  43.5 percent of their former net income in case of unemployment and the employment protection (mean value of 1.5) is also very low. 
+  #  low government expenditures on social security
+  #  low tax revenues can be described as a strategy aiming to improve price competitiveness.
+  cluster_3_vars <- c(
+    "coal_metal_export_share", 
+    "ict_ksh",
+    "employment_protect", "gini_market", "ubr"
+  )
+  
+  # Cluster 4: less globalised model (Poland, Greece, Portugal, Slovakia, Spain, Italia, Slovenia and Czech Republic)
+  #  exact counterpart to cluster 2 (see Table 2)
+  #  low degree of international integration as reflected by low values in foreign direct investments and the economic globalisation index. 
+  #  government expenditures on education and investments in research and development are considerably below the other trade regimes. 
+  #  relatively strict labor market regulation.
+  cluster_4_vars <- c(
+    "kof_econ_defacto", 
+    "gov_exp_educ",
+    "employment_protect",
+    "kof_econ_dejure", "fdi_to_gdp"
+  )
+  
+  # Cluster 5: Technological leaders (France, Sweden, Finland, Denmark, Netherlands, Belgium, Germany and Austria)
+  #  highest degree of economic complexity
+  #  high degree of wage coordination and high government expenditures on social security.
+  #  high investments in research and development
+  cluster_5_vars <- c(
+    "complexity_harv", "gerd", "industrial_to_gdp", 
+    "coord", "gov_exp_socprtc"
+  )
+  
+  clustering_vars <- list(
+    "C1" = cluster_1_vars,
+    "C2" = cluster_2_vars,
+    "C3" = cluster_3_vars,
+    "C4" = cluster_4_vars,
+    "C5" = cluster_5_vars
+  )
+  
+  for (i in 1:5){
+    cluster_considered <- paste0("C", i)
+    print(cluster_considered)
+    current_plots <- make_plots(data_taxonomy, cluster_considered, clustering_vars)
+    ggsave(plot = current_plots, 
+           filename = paste0("output/", cluster_considered, "_barplot.pdf"), 
+           width = length(current_plots)*2.5, height = 8)
+  }
 }
